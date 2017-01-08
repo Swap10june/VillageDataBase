7f591becfa488d84c33433af2f26651f9b93438b
swapnil.jagadale <swapnil.jagadale@unityinfotechgroup.in>
2017-01-08 14:19:06 +0530

    # Adding Validation in Add Member Button

--- src/handlers/SubmitMemberButtonHandler.java
+++ src/handlers/SubmitMemberButtonHandler.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 import beans.SMember;
 import src.Test;
@@ -26,26 +27,38 @@ public class SubmitMemberButtonHandler implements ActionListener {
 	{
 		if(e.getActionCommand().equalsIgnoreCase("Add Family Head"))
 		{
-
-			//SMember member = (SMember) Test.main.get(addMember.txtAssignMemberID.getText());
-			
-			SMember member = new SMember();
-			member.setMember_id(addMember.txtAssignMemberID.getText());
-			member.setM_name_e(addMember.getTxtNameE().getText());
-			member.setM_name_m(addMember.getTxtNameM().getText());
-			member.setFamily_head_status(addMember.getComboHeadStatus().getSelectedItem().toString());
-			member.setM_sex(addMember.getComboSexStatus().getSelectedItem().toString());
-			member.setM_state(addMember.getComboStateStatus().getSelectedItem().toString());
-			member.setM_dist(addMember.getComboDistStatus().getSelectedItem().toString());
-			member.setM_tal(addMember.getTxtTalStatus().getText());
-			member.setM_gaon(addMember.getTxtVillageStatus().getText());
-			member.setM_dob(addMember.getTxtDobStatus().getText());
-			member.setM_ward(Integer.parseInt(addMember.getTxtWardStatus().getText()));
-			member.setM_contact(addMember.getTxtContStatus().getText());
-			addMember.setMember(member);
-			AddFamily.listOfHead.add(member.getMember_id()+">"+member.getM_name_e());
-			AddFamily.btnAddFamilyHead.setEnabled(false);
-			owner.dispose();
+			if(addMember.txtAssignMemberID.getText().isEmpty() || 
+					addMember.getTxtNameE().getText().isEmpty()||
+					addMember.getTxtNameM().getText().isEmpty() ||
+					addMember.getComboHeadStatus().getSelectedItem().toString().isEmpty() ||
+					addMember.getComboSexStatus().getSelectedItem().toString().isEmpty() ||
+					addMember.getComboStateStatus().getSelectedItem().toString().isEmpty()
+					
+					)
+					
+			{
+				JOptionPane.showMessageDialog (owner, "Please Fill All The Fields","Error", JOptionPane.ERROR_MESSAGE);
+			}
+			else
+			{
+				SMember member = new SMember();
+				member.setMember_id(addMember.txtAssignMemberID.getText());
+				member.setM_name_e(addMember.getTxtNameE().getText());
+				member.setM_name_m(addMember.getTxtNameM().getText());
+				member.setFamily_head_status(addMember.getComboHeadStatus().getSelectedItem().toString());
+				member.setM_sex(addMember.getComboSexStatus().getSelectedItem().toString());
+				member.setM_state(addMember.getComboStateStatus().getSelectedItem().toString());
+				member.setM_dist(addMember.getComboDistStatus().getSelectedItem().toString());
+				member.setM_tal(addMember.getTxtTalStatus().getText());
+				member.setM_gaon(addMember.getTxtVillageStatus().getText());
+				member.setM_dob(addMember.getTxtDobStatus().getText());
+				member.setM_ward(Integer.parseInt(addMember.getTxtWardStatus().getText()));
+				member.setM_contact(addMember.getTxtContStatus().getText());
+				addMember.setMember(member);
+				AddFamily.listOfHead.add(member.getMember_id()+">"+member.getM_name_e());
+				AddFamily.btnAddFamilyHead.setEnabled(false);
+				owner.dispose();
+			}
 		}
 		if(e.getActionCommand().equalsIgnoreCase("Add Member"))
 		{
